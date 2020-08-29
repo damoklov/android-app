b807d92386f7cdb7830a6ac4655ec9f43b96d3b8
Terry <taraskavkalo@gmail.com>
2020-08-30 00:41:56 +0300

    finish login page ui & add request sending logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(new MyApp());
 
@@ -7,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      theme: new ThemeData(),
+      debugShowCheckedModeBanner: false,
       home: new FormPage(),
     );
   }
@@ -19,91 +23,136 @@ class FormPage extends StatefulWidget {
 }
 
 class _FormPageState extends State<FormPage> {
-  final scaffoldKey = new GlobalKey<ScaffoldState>();
-  final formKey = new GlobalKey<FormState>();
+  String _email = "";
+  String _password = "";
 
-  String _email;
-  String _password;
+  //////////////////////////////////////////////////////////////
+  sendLogInRequest() async {
+    Map data = {"email": _email, "password": _password};
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-  }
+    final response =
+        await http.post("url_here",
+            headers: {
+              HttpHeaders.authorizationHeader: "your_api_token_here",
+              'Content-type': 'application/json',
+            },
+            body: jsonEncode(data));
 
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-  }
-
-  void _submit() {
-    final form = formKey.currentState;
-
-    if (form.validate()) {
-      form.save();
-
-      performLogin();
+    if (response.statusCode == 201) {
+      //return jsonDecode(response.body);
+      print("success");
+    } else {
+      print("failed log in");
     }
   }
+  /////////////////////////////////////////////////////////////////
 
-  void performLogin() {
-    final snackbar = new SnackBar(
-      content: new Text("Email : $_email, password : $_password"),
+  letsGetStartedTextConfig() {
+    return Container(
+      child: Stack(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.fromLTRB(10, 140, 0, 0),
+            child: Text(
+              "LET'S GET\nSTARTED",
+              style: TextStyle(
+                  fontSize: 60,
+                  fontFamily: "LaranjhaLight",
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xffE4E4E4)),
+            ),
+          )
+        ],
+      ),
     );
-    scaffoldKey.currentState.showSnackBar(snackbar);
+  }
+
+  emailTextFieldConfig() {
+    return TextField(
+      onTap: ,
+      onChanged: (text) => _email = text,
+      textAlign: TextAlign.center,
+      cursorColor: Color(0xff464950),
+      style: TextStyle(color: Color(0xff464950)),
+      decoration: InputDecoration(
+        enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Color(0xff8F95A3))),
+        focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Color(0xff000000))),
+        labelText: "email",
+        labelStyle: TextStyle(
+            fontSize: 21, fontFamily: "GothamPro", color: Color(0xff6E6E6E)),
+      ),
+    );
+  }
+
+  passwordTextFieldConfig() {
+    return TextField(
+      onChanged: (text) => _password = text,
+      textAlign: TextAlign.center,
+      cursorColor: Color(0xff464950),
+      style: TextStyle(color: Color(0xff464950)),
+      obscureText: true,
+      decoration: InputDecoration(
+        enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Color(0xff8F95A3))),
+        focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Color(0xff000000))),
+        labelText: "password",
+        labelStyle: TextStyle(
+            fontSize: 21, fontFamily: "GothamPro", color: Color(0xff6E6E6E)),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        key: scaffoldKey,
-        appBar: new AppBar(
-          title: new Text("Form Page"),
-        ),
-        body: new Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: new Form(
-            key: formKey,
-            child: new Column(
-              children: <Widget>[
-                new TextFormField(
-                  decoration: new InputDecoration(labelText: "Email"),
-                  validator: (val) =>
-                  !val.contains('@') ? 'Invalid Email' : null,
-                  onSaved: (val) => _email = val,
-                ),
-                new TextFormField(
-                  decoration: new InputDecoration(labelText: "Password"),
-                  validator: (val) =>
-                  val.length < 6 ? 'Password too short' : null,
-                  onSaved: (val) => _password = val,
-                  obscureText: true,
-                ),
-                new Padding(
-                  padding: const EdgeInsets.only(top: 20.0),
-                ),
-                new RaisedButton(
-                  child: new Text(
-                    "login",
-                    style: new TextStyle(color: Colors.white),
+      resizeToAvoidBottomPadding: false,
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/login_form.png"),
+                fit: BoxFit.cover)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            letsGetStartedTextConfig(),
+            Container(
+              padding: EdgeInsets.only(top: 150, left: 60, right: 60),
+              child: Column(
+                children: <Widget>[
+                  emailTextFieldConfig(),
+                  passwordTextFieldConfig(),
+                  SizedBox(height: 40),
+                  Container(
+                    padding: EdgeInsets.only(top: 30, left: 10, right: 10),
+                    height: 90,
+                    child: Material(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Color(0xffFE862C),
+                      child: GestureDetector(
+                        onTap: () => sendLogInRequest(),
+                        child: Center(
+                          child: Text(
+                            "Log in",
+                            style: TextStyle(
+                              fontSize: 21,
+                              fontFamily: "GothamPro",
+                              color: Color(0xffFFFFFF),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
-                  color: Colors.blue,
-                  onPressed: _submit,
-                )
-              ],
-            ),
-          ),
-        ));
+                  SizedBox(height: 20)
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
-
-//        body: Container(
-//          decoration: BoxDecoration(
-//            image: DecorationImage(
-//              image: AssetImage("assets/images/login_form.png"),
-//              fit: BoxFit.cover,
-//            )
-//          ),
-//
-//        )
